622c9ebbf1a5a2f2a41492161e3a2247e689f5df
AlanRicardoS <alan.ricardo.silva@hotmail.com>
2019-11-04 15:02:35 -0300

    completo

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -2,7 +2,11 @@ from flask import render_template
 from app import app
 
 
-@app.route("/", defaults={"user":None}) #define uma rota da pagina. No caso a principal 
-@app.route("/index/<user>")
+@app.route("/") #define uma rota da pagina. No caso a principal 
+@app.route("/index/")
 def index():
     return render_template("index.html")
+
+@app.route("/teste/")
+def teste():
+    return render_template("teste.html",user="User")
--- config.py
+++ config.py
@@ -1,1 +1,4 @@
-#dçslfjghçdfjklghlksdfjghlsdfkjghldsfkjgh
+#DEBUG = True
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///storage.db'
+SQLALCHEMY_TRACK_MODIFICATIONS = True
